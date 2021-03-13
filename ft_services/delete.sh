92d8de471bc3f128ab2af506dc32983e78454d7f
demian2435 <demian2435@gmail.com>
2021-03-13 15:25:22 +0100

    add nginx to wordpress

--- ft_services/delete.sh
+++ ft_services/delete.sh
@@ -10,10 +10,10 @@
 #docker rm $(docker ps -a -f status=exited -f status=created -q)
 #docker rmi $(docker images -a -q)
 
-kubectl delete -f srcs/metallb.yaml 
-kubectl delete -f srcs/mysql.yaml 
-kubectl delete -f srcs/nginx.yaml
-kubectl delete -f srcs/phpmyadmin.yaml
+#kubectl delete -f srcs/metallb.yaml 
+#kubectl delete -f srcs/mysql.yaml 
+#kubectl delete -f srcs/nginx.yaml
+#kubectl delete -f srcs/phpmyadmin.yaml
 kubectl delete -f srcs/wordpress.yaml
-kubectl delete -f srcs/ftps.yaml
-kubectl delete -f srcs/influxdb.yaml
+#kubectl delete -f srcs/ftps.yaml
+#kubectl delete -f srcs/influxdb.yaml
--- ft_services/setup.sh
+++ ft_services/setup.sh
@@ -1,24 +1,24 @@
-minikube stop
-minikube delete --all --purge
-minikube --driver=docker start
+#minikube stop
+#minikube delete --all --purge
+#minikube --driver=docker start
 minikube addons enable metrics-server
 minikube addons enable dashboard
 minikube addons enable metallb
 kubectl apply -f srcs/metallb.yaml
 eval $(minikube docker-env)
 
-docker build -t nginx-img srcs/nginx/. && kubectl apply -f srcs/nginx.yaml
+#docker build -t nginx-img srcs/nginx/. && kubectl apply -f srcs/nginx.yaml
 
-docker build -t mysql-img srcs/mysql/. && kubectl apply -f srcs/mysql.yaml
+#docker build -t mysql-img srcs/mysql/. && kubectl apply -f srcs/mysql.yaml
 
-docker build -t phpmyadmin-img srcs/phpmyadmin/. && kubectl apply -f srcs/phpmyadmin.yaml
+#docker build -t phpmyadmin-img srcs/phpmyadmin/. && kubectl apply -f srcs/phpmyadmin.yaml
 
 docker build -t wordpress-img srcs/wordpress/. && kubectl apply -f srcs/wordpress.yaml
 
-docker build -t ftps-img srcs/ftps/. && kubectl apply -f srcs/ftps.yaml
+#docker build -t ftps-img srcs/ftps/. && kubectl apply -f srcs/ftps.yaml
 
-docker build -t influxdb-img srcs/influxdb/. && kubectl apply -f srcs/influxdb.yaml
+#docker build -t influxdb-img srcs/influxdb/. && kubectl apply -f srcs/influxdb.yaml
 
 
 #SHELL
-#kubectl exec --stdin --tty mysql -- /bin/bash
+#kubectl exec --stdin --tty mysql -- sh
