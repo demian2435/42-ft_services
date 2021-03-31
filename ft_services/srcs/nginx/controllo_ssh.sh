24439af120fcaa2b0ac4b85ed77a8da4c919849f
demian2435 <demian2435@gmail.com>
2021-03-31 15:36:04 +0200

    controllo servizi ok

--- ft_services/srcs/nginx/controllo_ssh.sh
+++ ft_services/srcs/nginx/controllo_ssh.sh
@@ -2,4 +2,7 @@ while true;do
     instan=$(pgrep -f sshd:)
 	[[ "$(echo "$instan")" == '' ]] && kill $(pgrep -f "nginx: master") && exit
 	sleep 5s
+    instan=$(pgrep -f nginx)
+	[[ "$(echo "$instan")" == '' ]] && exit
+	sleep 5s
 done
